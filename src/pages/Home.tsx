bfda82511dba4265abdec809b45fed66dfe0bb24
A Holcsik <you@example.com>
2023-09-20 21:21:42 +0200

    list by type added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,29 +6,37 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     const [loading, setLoading] = useState(false)
     const [pokeType, setPokeType] = useState <any []>([])
+    const [pokemonByType, setByType] = useState <any []>([])
 
     useEffect(() => {
-        setLoading(true)
-        axios
-          .get('https://pokeapi.co/api/v2/type')
-          .then((response) => {
-              setPokeType(response.data.results)
-              setLoading(false)
-          })
-          .catch((error) => {
-            console.log(error)
+      setLoading(true)
+      axios
+        .get('https://pokeapi.co/api/v2/type')
+        .then((response) => {
+            setPokeType(response.data.results)
             setLoading(false)
-          })
-    }, [
-      console.log(pokeType)
-    ])
+        })
+        .catch((error) => {
+          console.log(error)
+          setLoading(false)
+        })
+    }, [])
+
+    const selectPokeType = (url: string) => {
+      axios
+        .get(url)
+        .then((response) => {
+          setByType(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
     
   return (
     <div className='p-4'>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Pokedex</h1>
-        {/* <Link to='create'>
-        </Link> */}
       </div>
       {loading ? (
         <Spinner />
@@ -37,14 +45,28 @@ const Home = () => {
           <h3>Pokemon types</h3>
           <div className='flex flex-wrap my-4'>
             {pokeType.map((type) => (
-              <span className='border border-slate-600 rounded-md m-2 p-4 hover:bg-violet-100'>{type.name}</span>
+              <button
+                className='border border-slate-600 rounded-md m-2 p-4 hover:bg-violet-100 w-[100px]'
+                onClick={() => selectPokeType(type.url)}
+              >
+                {type.name}
+              </button>
             ))}
-
           </div>
         </div>
-
       )}
-
+      {pokemonByType.length !== 0 &&
+        <div>
+          <h3>Pokemon of {pokemonByType.name} type</h3>
+          {pokemonByType.pokemon.map((pokemon) => (
+            <button
+              className='border border-slate-600 rounded-md m-2 p-4 hover:bg-sky-100 w-[100px]'
+            >
+              {pokemon.pokemon.name}
+            </button>
+          ))}
+        </div>
+      }
     </div>
   )
 }
